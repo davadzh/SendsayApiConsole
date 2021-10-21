b3ee9b77dd2a27c7871fd5d0afc37a3dcd8cd75c
David <adzhamyand@gmail.com>
2021-10-21 12:37:25 +0300

    ref: add hashrouter

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/api-console/">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />

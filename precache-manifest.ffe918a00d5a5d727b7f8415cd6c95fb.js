ae2cc4ec0a407cf390e790fc92b98816996d252f
David <adzhamyand@gmail.com>
2021-10-21 12:18:03 +0300

    Updates

---
+++ precache-manifest.ffe918a00d5a5d727b7f8415cd6c95fb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2299e953a320a396839c56f565ffdf",
+    "url": "/api-console/index.html"
+  },
+  {
+    "revision": "224447823b36f9c3c590",
+    "url": "/api-console/static/css/main.07aab29a.chunk.css"
+  },
+  {
+    "revision": "f740b84d6fae12a5deab",
+    "url": "/api-console/static/js/2.c970f6d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/api-console/static/js/2.c970f6d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224447823b36f9c3c590",
+    "url": "/api-console/static/js/main.10fd5b74.chunk.js"
+  },
+  {
+    "revision": "4d20d334907fa35e0959",
+    "url": "/api-console/static/js/runtime-main.5793e4b0.js"
+  },
+  {
+    "revision": "ce051b7baab41f670dbc508d858a87c8",
+    "url": "/api-console/static/media/cross.ce051b7b.svg"
+  },
+  {
+    "revision": "81115c8152d83912e785379cbba12159",
+    "url": "/api-console/static/media/dots.81115c81.svg"
+  },
+  {
+    "revision": "aeb66b10d516e02c84324e3bfa1b8e4d",
+    "url": "/api-console/static/media/failure-face.aeb66b10.svg"
+  },
+  {
+    "revision": "7ac10753c361ca10f86b3dbc00113188",
+    "url": "/api-console/static/media/format.7ac10753.svg"
+  },
+  {
+    "revision": "8f9c9ecdcf34868bad085fcc40d00b22",
+    "url": "/api-console/static/media/full-screen-enabled.8f9c9ecd.svg"
+  },
+  {
+    "revision": "aea039bebc26b07a8f122876b4093f25",
+    "url": "/api-console/static/media/full-screen.aea039be.svg"
+  },
+  {
+    "revision": "35b4cb4ce092ba46277d436a547b29fe",
+    "url": "/api-console/static/media/loader.35b4cb4c.svg"
+  },
+  {
+    "revision": "6fb60e45ff3a63ab6cf6c05a9bf7b2ef",
+    "url": "/api-console/static/media/log-out.6fb60e45.svg"
+  },
+  {
+    "revision": "2140c08d548b77cd76bb98508e6ac075",
+    "url": "/api-console/static/media/logo.2140c08d.svg"
+  }
+]);
